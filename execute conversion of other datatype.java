cb78b3ceabd637e51097565653ffc5364ba74a22
Harishwaran2002 <146076518+Harishwaran2002@users.noreply.github.com>
2024-07-23 21:50:50 +0530

    execute conversion of other datatype.java

---
+++ execute conversion of other datatype.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n1;
+        n1=sc.nextInt();
+        char ch=(char)n1;
+        System.out.println(ch);
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
